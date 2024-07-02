2d09516ee2a1ff37e461faa2df75a1522324cf2b
glor-w <75494037+glor-w@users.noreply.github.com>
2024-07-02 11:44:34 -0400

    [ESW-591] Integrate Updated Bit Reduction Implementation (#4)
    * add dummy inputs to read only one channel of csv
    
    change ecg_reduced data to int16_t from uint16_t
    
    * update coefficients in bit reduction
    
    * use digital_filter with floats instead
    
    * print all three channel outputs from main.c
    
    * update bit reduc to use float
    
    add floorf in lsbremoval
    fix reset filter logic to match python
    renamed some variables in reset filter logic
    removed csv_writers from the includes
    
    * remove br filter from data_processing
    
    swap steps 6 and 7 in the filter
    
    * add comment to clarify when to use bufferrefresh
    
    remove printout from ecg_bit_reduction.c
    
    * undo change to filter, since the change in step 5 when to do the shift
    
    * cap samples_since_last_high to avoid overflow
    
    rename br_threshold_amp_settling to match python
    
    * formatting fixes
    
    made tmp = tmp/pA[0] to be more clear
    fixed comments use /* except after #defines
    removed csv_writers.h from data_processing.c

--- shared/data_processing.c
+++ shared/data_processing.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 float digital_filter(float dInput, float *pIn, float *pOut, const float *pA, const float *pB, uint8_t aLength, uint8_t bLength, uint8_t bFilterOrder, bool fReset, float dInitialSample)
@@ -9,109 +10,59 @@ float digital_filter(float dInput, float *pIn, float *pOut, const float *pA, con
     float output = 0.0f;
     float tmp    = 0.0f;
 
-    // 1) Check arguments - pointers
+    /* 1) Check arguments - pointers */
     if ((pA == NULL) || (pB == NULL) || (pIn == NULL) || (pOut == NULL))
     {
         return 0;
     }
 
-    // 2) Check arguments - lengths
+    /* 2) Check arguments - lengths */
     if ((bFilterOrder == 0) || (aLength == 0) || (bLength == 0) || (aLength > bFilterOrder) || (bLength > bFilterOrder))
     {
         return 0;
     }
 
-    // 3) If fReset was set, modify input data
+    /* 3) If fReset was set, modify input data */
     if (fReset)
     {
         for (uint8_t i = 0; i < bFilterOrder; i++)
         {
-            pIn[i]  = dInitialSample;   // Initialize input buffer with initial sample
-            pOut[i] = 0;                // Initialize output buffer with zero
+            /* Initialize input buffer with initial sample */
+            pIn[i] = dInitialSample;
+
+            /* Initialize output buffer with zero */
+            pOut[i] = 0;
         }
     }
-    pIn[bFilterOrder - 1] = dInput;     // Place new input sample at the end of input buffer
 
-    // 4) Apply the feedforward coefficients to the input samples 
+    /* Place new input sample at the end of input buffer */
+    pIn[bFilterOrder - 1] = dInput;
+
+    /* 4) Apply the feedforward coefficients to the input samples */
     for (uint8_t i = 0; i < bLength; i++)
     {
         tmp += pB[i] * pIn[bFilterOrder - 1 - i];
     }
 
-    // 5) Apply the feedback coefficients to the output samples
+    /* 5) Apply the feedback coefficients to the output samples */
     for (uint8_t i = 1; i < aLength; i++)
     {
         tmp -= pA[i] * pOut[bFilterOrder - 1 - i];
     }
 
-    // 6) Normalize by the first feedback coefficient 
-    tmp /= pA[0];
-    output = tmp;
-    pOut[bFilterOrder - 1]  = output;
+    /* 6) Normalize by the first feedback coefficient */
+    tmp                    = tmp / pA[0];
+    output                 = tmp;
+    pOut[bFilterOrder - 1] = output;
 
-    // 7) Shift input and output buffers to make room for the next sample
+    /* 7) Shift input and output buffers to make room for the next sample */
     for (uint8_t i = 1; i < bFilterOrder; i++)
     {
         pIn[i - 1]  = pIn[i];
         pOut[i - 1] = pOut[i];
     }
 
-    // 8) Return the filtered output sample
+    /* 8) Return the filtered output sample */
     return output;
 }
 
-double ecgbr_digital_filter(double dInput, double *pIn, double *pOut, const double *pA, const double *pB, uint8_t aLength, uint8_t bLength, uint8_t bFilterOrder, bool fReset, double dInitialSample)
-{    
-    double output = 0.0f;
-    double tmp    = 0.0f;
-
-    // 1) Check arguments - pointers
-    if ((pA == NULL) || (pB == NULL) || (pIn == NULL) || (pOut == NULL))
-    {
-        return 0;
-    }
-
-    // 2) Check arguments - lengths
-    if ((bFilterOrder == 0) || (aLength == 0) || (bLength == 0) || (aLength > bFilterOrder) || (bLength > bFilterOrder))
-    {
-        return 0;
-    }
-
-    // 3) If fReset was set, modify input data
-    if (fReset)
-    {
-        for (uint8_t i = 0; i < bFilterOrder; i++)
-        {
-            pIn[i]  = dInitialSample;   // Initialize input buffer with initial sample
-            pOut[i] = 0;                // Initialize output buffer with zero
-        }
-    }
-    pIn[bFilterOrder - 1] = dInput;     // Place new input sample at the end of input buffer
-
-    // 4) Apply the feedforward coefficients to the input samples 
-    for (uint8_t i = 0; i < bLength; i++)
-    {
-        tmp += pB[i] * pIn[bFilterOrder - 1 - i];
-    }
-
-    // 5) Apply the feedback coefficients to the output samples
-    for (uint8_t i = 1; i < aLength; i++)
-    {
-        tmp -= pA[i] * pOut[bFilterOrder - 1 - i];
-    }
-
-    // 6) Normalize by the first feedback coefficient 
-    tmp /= pA[0];
-    output = tmp;
-    pOut[bFilterOrder - 1]  = output;
-
-    // 7) Shift input and output buffers to make room for the next sample
-    for (uint8_t i = 1; i < bFilterOrder; i++)
-    {
-        pIn[i - 1]  = pIn[i];
-        pOut[i - 1] = pOut[i];
-    }
-
-    // 8) Return the filtered output sample
-    return output;
-}
